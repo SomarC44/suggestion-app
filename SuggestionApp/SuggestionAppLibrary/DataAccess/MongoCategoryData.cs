8a422b0b73c434dfb4e2f47c8d40a64474d8e17b
SomarC44 <somarobeid@gmail.com>
2023-10-01 10:15:07 -0400

    1. add DataAccess class MongoSuggestionData     - add code to GetAllSuggestions     - add code to GetAllApprovedSuggestions     - add code to GetSuggestion     - add code to GetAllSuggestionsWaitingForApproval     - add code to UpdateSuggestion     - add code to UpvoteSuggestion     - add code to CreateSuggestion     - extract ISuggestionData interface from MongoSuggestionData
    2. add dependency injection and services to SuggestionAppUI in RegisterServices class

--- SuggestionApp/SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
+++ SuggestionApp/SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
@@ -6,7 +6,7 @@ namespace SuggestionAppLibrary.DataAccess
     {
         private readonly IMongoCollection<CategoryModel> _categories;
         private readonly IMemoryCache _cache;
-        private const string cacheName = "CategoryData";
+        private const string CacheName = "CategoryData";
 
         public MongoCategoryData(IDbConnection db, IMemoryCache cache)
         {
@@ -16,13 +16,13 @@ namespace SuggestionAppLibrary.DataAccess
 
         public async Task<List<CategoryModel>> GetAllCategories()
         {
-            var output = _cache.Get<List<CategoryModel>>(cacheName);
+            var output = _cache.Get<List<CategoryModel>>(CacheName);
             if (output == null)
             {
                 var results = await _categories.FindAsync(_ => true);
                 output = results.ToList();
 
-                _cache.Set(cacheName, output, TimeSpan.FromDays(1));
+                _cache.Set(CacheName, output, TimeSpan.FromDays(1));
             }
 
             return output;
