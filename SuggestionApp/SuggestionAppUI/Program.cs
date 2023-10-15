e09d09b75e26e4bf8cad1a48dbef735c932ae6ab
SomarC44 <somarobeid@gmail.com>
2023-10-15 19:22:46 -0400

    - add AzureAdB2C section in appsettings.json - configure program.cs to use Authentication - add AddAuthentication service to RegisterServices.cs - add AdminPolicy - add UseRewriter in program.cs to redirect to home when signing out - add CascadingAuthenticationState to App.razor

--- SuggestionApp/SuggestionAppUI/Program.cs
+++ SuggestionApp/SuggestionAppUI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Rewrite;
 using SuggestionAppUI;
 
 namespace SuggestionAppUI
@@ -27,6 +28,20 @@ namespace SuggestionAppUI
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseRewriter(
+                new RewriteOptions().Add(
+                    context =>
+                    {
+                        if (context.HttpContext.Request.Path == "/MicrosoftIdentity/Account/SignedOut")
+                        {
+                            context.HttpContext.Response.Redirect("/");
+                        }
+                    }
+                    ));
 
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
